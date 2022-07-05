10cbc541cccb4843fbb8420a15f72b9b44c239c8
jctalavan <jctalavancanelo@gmail.com>
2022-07-05 20:05:15 +0200

    Finished step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary 
+ * data to each component using props. Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the
+ * total number of exercises.
+ */
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.course}</h1>
+  )
 }
 
-export default App;
+const Content = (props) => {
+  
+  const {part} = props
+  const {exercises} = props
+  
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Total = ({exercises1, exercises2, exercises3}) => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1}  exercises2={exercises2}  exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
